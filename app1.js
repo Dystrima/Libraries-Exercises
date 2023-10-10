db4727a5c0b401f38315a5361904fc8b12b2555c
Dystrima <138579225+Dystrima@users.noreply.github.com>
2023-10-10 13:24:09 +0200

    Update

---
+++ app1.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 3.5,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
